8bbfd420cc949b7b58bd06cbb449cb23d340dd6f
Juan Valencia <johnjuan15@gmail.com>
2023-04-18 23:18:15 -0700

    complete strings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'JUAN';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
